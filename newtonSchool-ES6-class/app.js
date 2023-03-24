9059ad486a3e2af5281c707eb75816d3a90c2c4d
abdurrahman6489 <abdurrahman489@gmail.com>
2023-03-24 16:51:34 +0530

    pushing the code for API class

---
+++ newtonSchool-ES6-class/app.js
@@ -0,0 +1,19 @@
+class API{
+    #secure = false;
+    url;
+    method = 'GET';
+    constructor(url){
+        this.url = url;
+    }
+    isSecure(){
+        return (this.secure===true);
+    }
+    updateUrl(url){
+        this.url = url;
+        let urlArray = url.split(":");
+        if(urlArray[0]==="https")
+            this.secure = true;
+        else if(urlArray[0]==="http")
+            this.secure = false;
+    }
+}
